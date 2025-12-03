5f10423d0f89b0a08ad3d9fc7c680f42857d6194
AlexMacor <alessio.macorano@unimi.it>
2025-12-03 14:05:16 +0100

    Remove unnecessary call to activate script

--- Inference/2D/Run_inference_gin_rdkit.cmd
+++ Inference/2D/Run_inference_gin_rdkit.cmd
@@ -1,8 +1,6 @@
 @echo off
 echo Running ..
 
-call "\Scripts\activate.bat"
-
 python Dealylation.py --root_path "main folder" --file_sdf_inf_p "sdf folder inference"
 python Glucuronidation.py --root_path "main folder" --file_sdf_inf_p "sdf folder inference"
 python GlutathioneConjugation.py --root_path "main folder" --file_sdf_inf_p "sdf folder inference"
@@ -10,3 +8,4 @@ python Hydrolysis.py --root_path "main folder" --file_sdf_inf_p "sdf folder infe
 python Oxidation.py --root_path "main folder" --file_sdf_inf_p "sdf folder inference"
 python Reduction.py --root_path "main folder" --file_sdf_inf_p "sdf folder inference"
 python Sulfonation.py --root_path "main folder" --file_sdf_inf_p "sdf folder inference"
+
