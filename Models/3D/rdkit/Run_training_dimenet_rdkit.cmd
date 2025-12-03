62a88140fce87aa0681b8442c8d9000e639eb972
AlexMacor <alessio.macorano@unimi.it>
2025-12-03 14:34:39 +0100

    Revise training script for dimenet with rdkit
    Updated training script to reflect new process and added Sulfonation script.

--- Models/3D/rdkit/Run_training_dimenet_rdkit.cmd
+++ Models/3D/rdkit/Run_training_dimenet_rdkit.cmd
@@ -1,12 +1,7 @@
 @echo off
-echo Iniziato il training per le reti dimenet con i descrittori rdkit
-
-call "path_to_env\Scripts\activate.bat"
-
-REM --root_path e' il percorso che contiene le cartelle con all'interno i file sdf 
+echo Training dimenet for GNN with rdkit
 
 python Dealylation.py --root_path "Dealkylation" > log_Dealylation_script.txt 2>&1
-python Equilibrium.py --root_path "Equilibrium" > log_Equilibrium_script.txt 2>&1
 python Glucuronidation.py --root_path "Glucuronidation" > log_Glucuronidation_script.txt 2>&1
 python GlutathioneConjugation.py --root_path "GlutathioneConjugation" > log_Glutathione_script.txt 2>&1
 python Hydrolysis.py --root_path "Hydrolysis" > log_Hydrolysis_script.txt 2>&1
