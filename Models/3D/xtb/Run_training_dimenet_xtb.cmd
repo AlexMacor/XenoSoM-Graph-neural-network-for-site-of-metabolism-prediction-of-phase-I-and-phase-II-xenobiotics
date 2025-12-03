6fba59928b5c8124807e02e1e40894023e32927e
AlexMacor <alessio.macorano@unimi.it>
2025-12-03 14:31:58 +0100

    Update Run_training_dimenet_xtb.cmd

--- Models/3D/xtb/Run_training_dimenet_xtb.cmd
+++ Models/3D/xtb/Run_training_dimenet_xtb.cmd
@@ -1,16 +1,10 @@
 @echo off
-echo Iniziato il training per le reti dimenet con i descrittori cdk_xtb 
+echo Training dimenet with cdk and xtb 
 
-call "path_to_env\Scripts\activate.bat"
-
-REM --root_path e' il percorso che contiene le cartelle con all'interno i file sdf 
-REM --xtb_path e' il percorso che contiene le cartelle con all'interno i file csv di output del calcolo xtb 
-
-python Dealylation.py --root_path "Dealkylation" --xtb_path "\percorso\descrittori\xtb" > log_Dealylation_script_xtb.txt 2>&1
-python Equilibrium.py --root_path "Equilibrium" --xtb_path "\percorso\descrittori\xtb" > log_Equilibrium_script_xtb.txt 2>&1
-python Glucuronidation.py --root_path "Glucuronidation" --xtb_path "\percorso\descrittori\xtb" > log_Glucuronidation_script_xtb.txt 2>&1
-python GlutathioneConjugation.py --root_path "GlutathioneConjugation" --xtb_path "\percorso\descrittori\xtb" > log_Glutathione_script_xtb.txt 2>&1
-python Hydrolysis.py --root_path "Hydrolysis" --xtb_path "\percorso\descrittori\xtb" > log_Hydrolysis_script_xtb.txt 2>&1
-python Oxidation.py --root_path "Oxidation" --xtb_path "\percorso\descrittori\xtb" > log_Oxidation_script_xtb.txt 2>&1
-python Reduction.py --root_path "Reduction" --xtb_path "\percorso\descrittori\xtb" > log_Reduction_script_xtb.txt 2>&1
-python Sulfonation.py --root_path "Sulfonation" --xtb_path "\percorso\descrittori\xtb" > log_Sulfonation_script_xtb.txt 2>&1
+python Dealylation.py --root_path "Dealkylation" --xtb_path "\descriptors\xtb" > log_Dealylation_script_xtb.txt 2>&1
+python Glucuronidation.py --root_path "Glucuronidation" --xtb_path "\descriptors\xtb" > log_Glucuronidation_script_xtb.txt 2>&1
+python GlutathioneConjugation.py --root_path "GlutathioneConjugation" --xtb_path "\descriptors\xtb" > log_Glutathione_script_xtb.txt 2>&1
+python Hydrolysis.py --root_path "Hydrolysis" --xtb_path "\descriptors\xtb" > log_Hydrolysis_script_xtb.txt 2>&1
+python Oxidation.py --root_path "Oxidation" --xtb_path "\descriptors\xtb" > log_Oxidation_script_xtb.txt 2>&1
+python Reduction.py --root_path "Reduction" --xtb_path "\descriptors\xtb" > log_Reduction_script_xtb.txt 2>&1
+python Sulfonation.py --root_path "Sulfonation" --xtb_path "\descriptors\xtb" > log_Sulfonation_script_xtb.txt 2>&1
