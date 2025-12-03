61ea9865659a5f7227830c713e5970d9d2add74e
AlexMacor <alessio.macorano@unimi.it>
2025-12-03 12:49:23 +0100

    Add files via upload

---
+++ Models/3D/cdk/modules/triplets.py
@@ -0,0 +1,43 @@
+import os
+import os.path as osp
+from functools import partial
+from math import pi as PI
+from math import sqrt
+from typing import Callable, Dict, Optional, Tuple, Union
+
+import numpy as np
+import torch
+from torch import Tensor
+from torch.nn import Embedding, Linear
+
+from torch_geometric.data import Dataset, download_url
+from torch_geometric.nn import radius_graph
+from torch_geometric.nn.inits import glorot_orthogonal
+from torch_geometric.nn.resolver import activation_resolver
+from torch_geometric.typing import OptTensor, SparseTensor
+from torch_geometric.utils import scatter
+
+def triplets(
+    edge_index: Tensor,
+    num_nodes: int,
+) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+    row, col = edge_index  # j->i
+
+    value = torch.arange(row.size(0), device=row.device)
+    adj_t = SparseTensor(row=col, col=row, value=value,
+                         sparse_sizes=(num_nodes, num_nodes))
+    adj_t_row = adj_t[row]
+    num_triplets = adj_t_row.set_value(None).sum(dim=1).to(torch.long)
+
+    # Node indices (k->j->i) for triplets.
+    idx_i = col.repeat_interleave(num_triplets)
+    idx_j = row.repeat_interleave(num_triplets)
+    idx_k = adj_t_row.storage.col()
+    mask = idx_i != idx_k  # Remove i == k triplets.
+    idx_i, idx_j, idx_k = idx_i[mask], idx_j[mask], idx_k[mask]
+
+    # Edge indices (k-j, j->i) for triplets.
+    idx_kj = adj_t_row.storage.value()[mask]
+    idx_ji = adj_t_row.storage.row()[mask]
+
+    return col, row, idx_i, idx_j, idx_k, idx_kj, idx_ji
