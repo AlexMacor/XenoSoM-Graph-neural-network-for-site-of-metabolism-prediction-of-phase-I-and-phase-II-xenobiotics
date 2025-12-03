fc1e583037772f543d0cb89f57d2805c765a038e
AlexMacor <alessio.macorano@unimi.it>
2025-12-03 12:50:11 +0100

    Add files via upload

---
+++ Models/3D/rdkit/modules/interaction_block.py
@@ -0,0 +1,90 @@
+import os
+import os.path as osp
+from functools import partial
+from math import pi as PI
+from math import sqrt
+from typing import Callable, Dict, Optional, Tuple, Union
+
+import numpy as np
+import torch
+from torch import Tensor
+from torch.nn import Embedding, Linear
+
+from torch_geometric.data import Dataset, download_url
+from torch_geometric.nn import radius_graph
+from torch_geometric.nn.inits import glorot_orthogonal
+from torch_geometric.nn.resolver import activation_resolver
+from torch_geometric.typing import OptTensor, SparseTensor
+from torch_geometric.utils import scatter
+from .residual_layer import ResidualLayer
+
+
+class InteractionBlock(torch.nn.Module):
+    def __init__(
+        self,
+        hidden_channels: int,
+        num_bilinear: int,
+        num_spherical: int,
+        num_radial: int,
+        num_before_skip: int,
+        num_after_skip: int,
+        act: Callable,
+    ):
+        super().__init__()
+        self.act = act
+
+        self.lin_rbf = Linear(num_radial, hidden_channels, bias=False)
+        self.lin_sbf = Linear(num_spherical * num_radial, num_bilinear,
+                              bias=False)
+
+        # Dense transformations of input messages.
+        self.lin_kj = Linear(hidden_channels, hidden_channels)
+        self.lin_ji = Linear(hidden_channels, hidden_channels)
+
+        self.W = torch.nn.Parameter(
+            torch.empty(hidden_channels, num_bilinear, hidden_channels))
+
+        self.layers_before_skip = torch.nn.ModuleList([
+            ResidualLayer(hidden_channels, act) for _ in range(num_before_skip)
+        ])
+        self.lin = Linear(hidden_channels, hidden_channels)
+        self.layers_after_skip = torch.nn.ModuleList([
+            ResidualLayer(hidden_channels, act) for _ in range(num_after_skip)
+        ])
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        glorot_orthogonal(self.lin_rbf.weight, scale=2.0)
+        glorot_orthogonal(self.lin_sbf.weight, scale=2.0)
+        glorot_orthogonal(self.lin_kj.weight, scale=2.0)
+        self.lin_kj.bias.data.fill_(0)
+        glorot_orthogonal(self.lin_ji.weight, scale=2.0)
+        self.lin_ji.bias.data.fill_(0)
+        self.W.data.normal_(mean=0, std=2 / self.W.size(0))
+        for res_layer in self.layers_before_skip:
+            res_layer.reset_parameters()
+        glorot_orthogonal(self.lin.weight, scale=2.0)
+        self.lin.bias.data.fill_(0)
+        for res_layer in self.layers_after_skip:
+            res_layer.reset_parameters()
+
+    def forward(self, x: Tensor, rbf: Tensor, sbf: Tensor, idx_kj: Tensor,
+                idx_ji: Tensor) -> Tensor:
+        rbf = self.lin_rbf(rbf)
+        sbf = self.lin_sbf(sbf)
+
+        x_ji = self.act(self.lin_ji(x))
+        x_kj = self.act(self.lin_kj(x))
+        x_kj = x_kj * rbf
+        x_kj = torch.einsum('wj,wl,ijl->wi', sbf, x_kj[idx_kj], self.W)
+        x_kj = scatter(x_kj, idx_ji, dim=0, dim_size=x.size(0), reduce='sum')
+
+        h = x_ji + x_kj
+        for layer in self.layers_before_skip:
+            h = layer(h)
+        h = self.act(self.lin(h)) + x
+        for layer in self.layers_after_skip:
+            h = layer(h)
+
+        return h
