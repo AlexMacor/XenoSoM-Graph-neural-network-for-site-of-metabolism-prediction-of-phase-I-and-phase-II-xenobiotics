591c26fd5a410173ed709a769eef22731087c0f3
AlexMacor <alessio.macorano@unimi.it>
2025-12-03 14:38:52 +0100

    Add files via upload

--- Models/3D/cdk/models_pytorch.py
+++ Models/3D/cdk/models_pytorch.py
@@ -43,9 +43,6 @@ from modules.output_pp_block import OutputPPBlock
 from modules.output_block import OutputBlock
 from modules.triplets import triplets
 
-### BATCH NORMALIZATION SOLO SE USO CDK E XTB DESCRITTORI NUMERICI
-
-## Qui la batch normalization viene rimossa 
 device = torch.device('cpu')
 
 n_0, n_1 = None, None
@@ -345,8 +342,7 @@ class DimeNetPP(DimeNet):
     def forward(self, z, pos, batch=None, data=None):    
         return super().forward(z, pos, batch=batch, data=data)
 
-
-# definisco il modello e importo   
+   
 def weighted_binary_crossentropy(y_true, y_pred):
     weight_0 = (n_0 + n_1) / (2 * n_0)
     weight_1 = (n_0 + n_1) / (2 * n_1)
