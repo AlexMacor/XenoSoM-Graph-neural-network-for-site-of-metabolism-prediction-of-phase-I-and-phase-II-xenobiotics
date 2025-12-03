c2bbba2fbc41b55927a9be5a7e574d295590919f
AlexMacor <alessio.macorano@unimi.it>
2025-12-03 14:35:44 +0100

    Update training script for dimenet with new echo and script
    Removed initial echo and added a new training echo message. Added a call to the Sulfonation.py script for processing.

--- Models/3D/cdk_xtb/Run_training_dimenet_cdk_xtb.cmd
+++ Models/3D/cdk_xtb/Run_training_dimenet_cdk_xtb.cmd
@@ -1,13 +1,7 @@
 @echo off
-echo Iniziato il training per le reti dimenet con i descrittori cdk_xtb 
-
-call "path_to_env\Scripts\activate.bat"
-
-REM --root_path e' il percorso che contiene le cartelle con all'interno i file sdf 
-REM --descr_path e' il percorso che contiene le cartelle con all'interno i file csv di output del calcolo cdk 
+echo Training dimenet using cdk and xtb descriptors
 
 python Dealylation.py --root_path "Dealkylation" --cdk_path "\percorso\descrittori\cdk" --xtb_path "\percorso\descrittori\xtb" > log_Dealylation_script_cdk_xtb.txt 2>&1
-python Equilibrium.py --root_path "Equilibrium" --cdk_path "\percorso\descrittori\cdk" --xtb_path "\percorso\descrittori\xtb" > log_Equilibrium_script_cdk_xtb.txt 2>&1
 python Glucuronidation.py --root_path "Glucuronidation" --cdk_path "\percorso\descrittori\cdk" --xtb_path "\percorso\descrittori\xtb" > log_Glucuronidation_script_cdk_xtb.txt 2>&1
 python GlutathioneConjugation.py --root_path "GlutathioneConjugation" --cdk_path "\percorso\descrittori\cdk" --xtb_path "\percorso\descrittori\xtb" > log_Glutathione_script_cdk_xtb.txt 2>&1
 python Hydrolysis.py --root_path "Hydrolysis" --cdk_path "\percorso\descrittori\cdk" --xtb_path "\percorso\descrittori\xtb" > log_Hydrolysis_script_cdk_xtb.txt 2>&1
