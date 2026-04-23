34f1b86e53e402db2eb8fbdb7af6bc1a6e12b322
AlexMacor <alessio.macorano@unimi.it>
2026-04-23 17:45:18 +0200

    Delete Descriptors directory

--- Descriptors/cdk/with-H/Cdk_calculation.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Running ..
-
-python sdf_process-cdk.py --multi_sdfs "C:\molecole\split_sdf" --cdk_jar_file "C:\cdk\cdk-2.2.jar" --multi_sdfs_out_cdk "C:\output\cdk_results" --csv_cdk_out "C:\output\nan_report.csv"
